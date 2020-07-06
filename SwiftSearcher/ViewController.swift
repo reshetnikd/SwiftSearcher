a179bfc220b8c880725ba94265481027dd538413
reshetnikd <dmitry.reshetnik@icloud.com>
2020-07-06 11:00:09 +0300

    Initial Commit

---
+++ SwiftSearcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSearcher
+//
+//  Created by Dmitry Reshetnik on 06.07.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
